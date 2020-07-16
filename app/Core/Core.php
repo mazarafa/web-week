5dbbf90d68bd0a38778de9bec0ac1165c53626a8
mazarafa <arruda.rafa.ra@gmail.com>
2020-07-16 16:25:35 -0400

    depois de apanhar muito, resolvi o trem de url

--- app/Core/Core.php
+++ app/Core/Core.php
@@ -1,16 +1,34 @@
 <?php
 
-    class Core
-    {
-        public function start($urlget)
-        {
-            $controller = ucfirst($urlget['pagina'].'Controller');
+	class Core
+	{
+		public function start($urlGet)
+		{
+			if (isset($urlGet['metodo'])) {
+				$acao = $urlGet['metodo'];
+			} else {
+				$acao = 'index';
+			}
 
-            if(!class_exists($controller)){
-                $controller = 'ErroController';
-            }
-            echo $controller;
+			if (isset($urlGet['pagina'])) {
+				$controller = ucfirst($urlGet['pagina'].'Controller');
+			} else {
+				$controller = 'HomeController';
+			}
+			
 
-            
-        }
-    }
+			if (!class_exists($controller)) {
+				$controller = 'ErroController';
+			}
+
+
+			if (isset($urlGet['id']) && $urlGet['id'] != null) {
+				$id = $urlGet['id'];
+			} else {
+				$id = null;
+			}
+
+			call_user_func_array(array(new $controller, $acao), array('id' => $id));
+
+		}
+	}
--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
     require_once 'app/Core/Core.php';
+
     require_once 'app/Controller/HomeController.php';
+    require_once 'app/Controller/ErroController.php';
 
 
     $template = file_get_contents('app/template/main.html');
     
-    $core = new Core;
-	$core->start($_GET);
-    //echo $template;
+    ob_start();
+        $core = new Core;
+        $core->start($_GET);
+
+        $saida = ob_get_contents();
+
+    ob_end_clean();
+    $tplPronto = str_replace('{{area_dinamica}}', $saida, $template);
+    echo $tplPronto;
 ?>
