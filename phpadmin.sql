5e1138102161835f9ed470861db7956137a72083
mazarafa <arruda.rafa.ra@gmail.com>
2020-07-20 15:30:28 -0400

    modelo do banco sql

---
+++ phpadmin.sql
@@ -0,0 +1,31 @@
+-- ===== Nome do banco 'week'; =======
+
+DROP TABLE IF EXISTS `postagem`;
+CREATE TABLE `postagem` (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,  
+  titulo varchar(200) NOT NULL,
+  conteudo text  NOT NULL
+);
+
+DROP TABLE IF EXISTS `comentario`;
+CREATE TABLE IF NOT EXISTS `comentario` (
+  `id` INTEGER AUTO_INCREMENT PRIMARY KEY,  
+  `nome` VARCHAR(50) NOT NULL,
+  `mensagem` TEXT NOT NULL,
+  `idPostagem` int(11),
+
+  FOREIGN KEY (idPostagem) REFERENCES postagem(id) 
+);
+
+
+-- postagem
+-- id int(11) primary key
+-- titulo varchar(200),
+-- conteudo text
+
+-- comentario
+-- id int(11) PRIMARY KEY
+-- nome varchar (200)
+-- mensagem text,
+-- id_postagem int(11)
+
